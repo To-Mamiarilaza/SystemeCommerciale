f321a6ed279c62ef900fb4217deb5cf7ca577720
nix19gremory <118509826+nix19gremory@users.noreply.github.com>
2023-11-20 07:13:57 +0100

    update

--- src/java/servlet/purchase/PurchaseRequestDetailServlet.java
+++ src/java/servlet/purchase/PurchaseRequestDetailServlet.java
@@ -14,7 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
-import java.util.List
+import java.util.List;
 import model.article.Article;
 import model.purchase.ArticleQuantity;
 import model.purchase.PurchaseRequest;
